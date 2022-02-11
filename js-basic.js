94b3f5e1dd36a2373ed7538fb1225eafc07c438b
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-02-12 09:58:06 +1100

    Display line 18

--- js-basic.js
+++ js-basic.js
@@ -4,3 +4,22 @@
 console.log("You are hacked");
 // creating variables
 // Rules to create the variables
+// Any varable to be created or defined, we need to use these keywords
+// var, let you change or override the data after it is created
+// const, you can not change the data after it is created
+/* In programming, we want to write the code from var or in other terms (ambiguity)
+to surety(predictability). Both take the same memory.*/
+
+var name = "John";
+let phone = 41444445;
+const dob = "12/12/20";
+// after line 15, go to the browser, click console, type name - "John" should appear
+
+console.log(name, phone, dob);
+// line 18 shows multiple variables written in one line
+// console.log(name);
+// console.log(phone);
+// console.log(name);
+// console.log(dob);
+
+// console is the object, and log is the method to print the item
